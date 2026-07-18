e42ee81ccb4c5f684bdbbeb0b7e5b4f487ccfd35
SATWIK <100929549+Sasuke66@users.noreply.github.com>
2026-07-18 13:36:54 +0000

    Time: 0 ms (100%), Space: 20.4 MB (95.62%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -2,7 +2,6 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left = 0
         right = len(nums) - 1
-
         while left <= right:
             mid = left + (right - left) // 2
 
@@ -12,5 +11,4 @@ class Solution:
                 left = mid + 1
             else:
                 right = mid -1
-
         return -1
